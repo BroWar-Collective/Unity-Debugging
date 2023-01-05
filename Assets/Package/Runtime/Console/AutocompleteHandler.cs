b263adc76fb91bbe453e3388b02f704ac6656a6e
Madoww <filiplszafran@gmail.com>
2023-01-06 00:01:31 +0100

    Added IAutocompleteProvider. Adding AutocompleteHandler and searching for matching names (in progress)

---
+++ Assets/Package/Runtime/Console/AutocompleteHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BroWar.Debugging.Console
+{
+    public class AutocompleteHandler
+    {
+        private IList<string> optionList = new List<string>();
+
+        public void RefreshOptions(IAutocompleteOptionProvider optionProvider, string[] words, int wordIndex)
+        {
+            optionList.Clear();
+
+            optionProvider.GetParemeterAutocompleteOptions(words, wordIndex, optionList);
+        }
+
+        public string GetBestMatch(string currentInput)
+        {
+            foreach (var option in optionList)
+            {
+                if (option.ToLower().Contains(currentInput.ToLower()))
+                {
+                    return option;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Assets/Package/Runtime/Console/IAutocompleteOptionProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BroWar.Debugging.Console
+{
+    public interface IAutocompleteOptionProvider
+    {
+        void GetParemeterAutocompleteOptions(string[] words, int wordIndex, IList<string> optionList);
+    }
+}
